caf830c5eeaa65042fb6a34e1ba1b55c8200f024
shan-qi <1241016810@qq.com>
2020-07-31 11:40:43 +0800

    拉取代码

---
+++ Desktop/flutter_ios/flutter_ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  flutter_ios
+//
+//  Created by 小琦 on 2020/7/31.
+//  Copyright © 2020 单琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Desktop/flutter_ios/flutter_ios/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  flutter_ios
+//
+//  Created by 小琦 on 2020/7/31.
+//  Copyright © 2020 单琦. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
